a581d48159eb6a1bd8f288266f14639ff17dd0da
emaged <emieldejonghswemer@gmail.com>
2026-01-23 01:41:47 +0100

    eslint-lsp, conform and nivm-lint updated

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -38,6 +38,8 @@ return {
       disabled = { -- disable formatting capabilities for the listed language servers
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
         "lua_ls",
+        "vtsls",
+        "eslint",
       },
       timeout_ms = 1000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
@@ -94,17 +96,6 @@ return {
           return vim.fs.dirname(fname)
         end,
         settings = {},
-      },
-
-      eslint = {
-        filetypes = {
-          "javascript",
-          "javascriptreact",
-          "mjs",
-          "cjs",
-          "html",
-          "css", -- ⭐ add this
-        },
       },
 
       -- Python LSP Configuration
--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,9 +2,13 @@ return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
-      html = { "prettierd", "prettier", stop_after_first = true },
-      css = { "prettierd", "prettier", stop_after_first = true },
-      javascript = { "prettierd", "prettier", stop_after_first = true },
+      css = { "prettierd" },
+      html = { "prettierd" },
+      javascript = { "prettierd" },
+      javascriptreact = { "prettierd" },
+      typescript = { "prettierd" },
+      typescriptreact = { "prettierd" },
+      json = { "prettierd" },
       lua = { "stylua" },
       htmldjango = { "djlint" },
     },
