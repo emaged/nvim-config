2c8150a9265f95a195ee8dc48d8bc1967ddec652
Emiel de Jongh Swemer <emieldejonghswemer@gmail.com>
2026-02-23 09:58:01 +0100

    set theme to catppuccin, no more switching

--- lua/plugins/deactivated.lua
+++ lua/plugins/deactivated.lua
@@ -3,6 +3,30 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 --
 return {
   --
+  -- omarchy color scheme switcher on startup, no hotreload
+  -- {
+  --   "AstroNvim/astroui",
+  --   opts = function(_, opts)
+  --     local theme_file = vim.fn.expand "~/.config/omarchy/current/theme/neovim.lua"
+  --     local ok, theme_spec = pcall(dofile, theme_file)
+  --
+  --     if not ok or type(theme_spec) ~= "table" then return end
+  --
+  --     for _, spec in ipairs(theme_spec) do
+  --       if type(spec) == "table" and spec[1] == "LazyVim/LazyVim" and spec.opts and spec.opts.colorscheme then
+  --         local cs = spec.opts.colorscheme
+  --
+  --         -- 🔥 Force mocha if catppuccin
+  --         if cs == "catppuccin" then cs = "catppuccin-mocha" end
+  --
+  --         opts.colorscheme = cs
+  --         break
+  --       end
+  --     end
+  --   end,
+  -- },
+  --
+  --
   -- "folke/sidekick.nvim",
   -- opts = {
   --   nes = { enabled = true },
